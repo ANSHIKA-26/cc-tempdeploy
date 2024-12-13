897ba0b17b7a7e520a95abb988b854468f62f757
ANSHIKA-26 <114569262+ANSHIKA-26@users.noreply.github.com>
2024-12-13 20:03:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,26 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 require("./db/conn");
-const path = require("path");
-
 const userRouter = require("./routes/userRoutes");
 const doctorRouter = require("./routes/doctorRoutes");
 const appointRouter = require("./routes/appointRoutes");
 const path = require("path");
 const notificationRouter = require("./routes/notificationRouter");
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH","OPTIONS"],
+    credentials: true
+  })
+);
+app.options('*', cors())
 
 // Hello endpoint
 app.get('/api/v1/hello', (req, res) => {
@@ -24,11 +30,7 @@ app.get('/api/v1/hello', (req, res) => {
       message: "Hello, the backend is working!",
     });
   });
-app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
   
 app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
